a3dc2ef5a3cc3a150030b41f133b3f0f0c0ac339
huangxw <huangxwabc@gmail.com>
2020-09-04 21:03:33 +0800

    [ansible-middle] 新增minio部分的内容，优化etcd的compact，增加hive2

--- bin/reinstall-hadoop.sh
+++ bin/reinstall-hadoop.sh
@@ -6,3 +6,7 @@ cd ${FWDIR}
 ansible-playbook 02.hadoop.yaml -t stop
 ansible-playbook 02.hadoop.yaml -t clean
 ansible-playbook 02.hadoop.yaml -t install
+
+ansible-playbook 02.hadoop.yaml -t install-root -e "ansible_become=true"
+ansible-playbook 02.hadoop.yaml -t format
+ansible-playbook 02.hadoop.yaml -t install-cgroup -e "ansible_become=true"
