adf2a2f5ad74661610acc8db7b047f04696f6cee
huangxw <huangxwabc@gmail.com>
2021-11-09 15:42:07 +0800

    [ansible-middle] 1.kafka ssl相关配置升级

--- bin/role-kafka2-ssl.sh
+++ bin/role-kafka2-ssl.sh
@@ -5,7 +5,6 @@ cd ${FWDIR}
 
 [ X"$*" == X"" ] && echo "Please write the parameters!" && exit 1
 
-# TODO sed -e部分
 IVPATH=$(cat ansible.cfg | grep "inventory = " | cut -d"=" -f2 | sed -e 's/^[ ]*//g' | sed -e 's/[ ]*$//g')
 C_IVPATH="${IVPATH}/kafka-ssl"
 echo "INVENTORY_PATH =" $C_IVPATH
