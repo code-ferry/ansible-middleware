db4b53933eabe10c80705608ff6a7e724a3a4dde
huangxw <huangxwabc@gmail.com>
2022-02-18 17:00:23 +0800

    [ansible-middle] 1.新增flume1.9

---
+++ bin/role-etcd-3.5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FWDIR="$(cd `dirname $0`/..; pwd)"
+
+[ X"$*" == X"" ] && echo "Please write the parameters!" && exit 1
+
+IVPATH=$(cat ansible.cfg | grep "inventory = " | cut -d"=" -f2 | sed -e 's/^[ ]*//g' | sed -e 's/[ ]*$//g')
+C_IVPATH="${IVPATH}/etcd-3.5"
+echo "INVENTORY_PATH =" $C_IVPATH
+
+ansible-playbook 03.etcd.yaml -i "${C_IVPATH}" $*
